6d57317729bf059523a9306f848f6befe85a7d44
kp6175858-bit <kp6175858@gmail.com>
2026-01-28 19:28:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById("aiBtn").addEventListener("click", () => {
-  alert("🤖 AI Mode Activated!\nStructure successfully rebuilt.");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
